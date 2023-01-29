4e453004595a9755ab8155cee5e9c6680726c734
ErshovAlexandr <116908300+ErshovAlexandr@users.noreply.github.com>
2023-01-29 20:40:17 +0300

    Initial Commit

---
+++ ImageFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by Александр Ершов on 29.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
